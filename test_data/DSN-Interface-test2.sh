71be5cad16dab691ac45a3a081d145cd03ec551a
saika2k <shiyanzjh0706@163.com>
2025-05-20 22:07:26 +0800

    update test scripts

--- test_data/DSN-Interface-test2.sh
+++ test_data/DSN-Interface-test2.sh
@@ -2,22 +2,21 @@
 
 ./DSN-Interface BFTUpload -file file6 -total 4
 sleep 600
-#Note: the CID of file1: bafykbzacebylwxfbdyazodwqqln5modpp6w32jzbn7p2pzh6qdy4b6zpcxyjc
+#Note: this command upload 4 shares to storage miners, the CID of each share are stored in the reference file
 #Note: we need to wait the storage miner to handle the deal
 
 ./DSN-Interface BFTRetrieve -file file6
 sleep 10
 ./lotus client import file6
 ./lotus client import recover_file6
-#Note: the CID of file1_v1 should be the same as file1
+#Note: the CID of recover_file6 should be the same as file6
 
 
 ########################################################################################
-#below is the content of the reference file of file1
-#bafykbzacebylwxfbdyazodwqqln5modpp6w32jzbn7p2pzh6qdy4b6zpcxyjc -1
-#bafykbzaceckdlpbjc4bf4fhyqb3m32p3uywebirqt6fewjweulyw4heuxuibc 1
-#bafykbzacedo2l4l4nu33dtnfnxqdi5gojpwgmmnu7il5zm2he4gipqvbdn2go 2
-#bafykbzaceb6djch625f4plidvoq2ahgh6mk4ixf2uurfmbwmfg3ssro5io4eo 3
-#bafykbzacedo3rbh6dtntzkftouwli5y3l4lwsjy7qpnyvdyrt7joowha75wpg 2
+#below is the content of the reference file of file6
+#bafykbzaceazlqdr37llxzp44nvcjwo4sshfd6os7gqygsukufyfliessqgkxu -1
+#bafykbzacecz6st37u37g6kabvysy2vvb6a7qrj6yljpikuljvbwfwhbrmacjm -1
+#bafykbzacebu5tr6fmj5yfmakeb7dwq3deam3cwssgyuysiwwdgxo25kkb7qds -1
+#bafykbzacecs3s3thza4ubyfcmo55vkgxbd4bpun56dkyuvrsyynkoo5pvz53a -1
 ########################################################################################
 
