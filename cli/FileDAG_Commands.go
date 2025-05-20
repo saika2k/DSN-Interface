0e64eaf24b225dbf78d6533520aa7212c6fe50b3
saika2k <shiyanzjh0706@163.com>
2025-05-20 14:08:29 +0800

    add the description of each command

---
+++ cli/FileDAG_Commands.go
@@ -0,0 +1,189 @@
+package cli
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+type fileRef struct {
+	fileCID         string
+	previousVersion int
+}
+
+func (cli *CLI) Upload(file string) {
+	dir, _ := os.Getwd()
+	referencePath := filepath.Join(dir, "reference", file)
+	fmt.Println(referencePath)
+	_, err := os.Stat(referencePath)
+
+	if err != nil {
+		//reference file not found, import it into FileDAG
+		cmd := exec.Command("./lotus", "client", "import", file)
+		out, _ := cmd.Output()
+		//The output of this command follows the format Import XXXXXX, Root CID, we now spilt the string to get the CID
+		parts := strings.Split(string(out), "Root ")
+		if len(parts) < 2 {
+			fmt.Println("Error importing file")
+			return
+		}
+		cid := strings.TrimSpace(parts[1])
+		fmt.Println(cid)
+		//use the CID of the file to upload it to the storage miner
+		cmd2 := exec.Command("./lotus", "client", "deal", cid, "t01000", "0.026", "518400")
+		out2, _ := cmd2.Output()
+		fmt.Println(string(out2))
+		//create the reference of the file to manage the state of the file
+		fileReference := []fileRef{}
+		fileReference = append(fileReference, fileRef{fileCID: cid, previousVersion: -1})
+		WriteReference(file, fileReference)
+		fmt.Println("Uploaded ", file, "(version 1) to storage miner.")
+	} else {
+		//reference file found, upload it to FileDAG
+		fmt.Println("the file ", file, "already exist in FileDAG. If you want to update it please use the update/fork/merge command.")
+	}
+}
+
+func (cli *CLI) Retrieve(file string, version int) {
+	dir, _ := os.Getwd()
+	referencePath := filepath.Join(dir, "reference", file)
+	fmt.Println(referencePath)
+	_, err := os.Stat(referencePath)
+
+	if err != nil {
+		//reference file not found, the user might input a wrong filename or need to upload the file first
+		fmt.Println("unable to retrieve the file ", file, "you might give a wrong filename or need to use the upload command to upload it.")
+	} else {
+		//open the reference file to get the CID and previousVersion of each patch of the file
+		filePrinter, _ := os.Open(referencePath)
+		defer filePrinter.Close()
+		//scan the file line by line and store it in an array
+		fileReference := []fileRef{}
+		scanner := bufio.NewScanner(filePrinter)
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+			parts := strings.Fields(line)
+			preVersion, _ := strconv.Atoi(parts[1])
+			reference := fileRef{fileCID: parts[0], previousVersion: preVersion}
+			fileReference = append(fileReference, reference)
+		}
+		//test scan
+		for i := 0; i < len(fileReference); i++ {
+			fmt.Println(fileReference[i].fileCID, fileReference[i].previousVersion)
+		}
+		//test scan
+		//Now retrieve the target versions
+		versionToRetrieve := version
+		totalRetrieve := 0
+		for {
+			if versionToRetrieve == -1 {
+				break
+			}
+			cidToRetrieve := fileReference[versionToRetrieve-1].fileCID
+			retrieveFileName := strconv.Itoa(totalRetrieve)
+			cmd := exec.Command("./lotus", "client", "retrieve", cidToRetrieve, retrieveFileName)
+			out, _ := cmd.Output()
+			//test command exec
+			fmt.Println(string(out))
+			//test command exec
+			versionToRetrieve = fileReference[versionToRetrieve-1].previousVersion
+			totalRetrieve += 1
+		}
+		//Finally, put the original version and patches together
+		if totalRetrieve == 1 {
+			//retrieve the original file, just rename the file
+			retrieveName := file + "_v" + strconv.Itoa(version)
+			os.Rename(strconv.Itoa(0), retrieveName)
+		} else {
+			tempFile := strconv.Itoa(rand.Int())
+			oldFile := strconv.Itoa(totalRetrieve - 1)
+			patchFile := strconv.Itoa(totalRetrieve - 2)
+			for {
+				cmd2 := exec.Command("bspatch", oldFile, tempFile, patchFile)
+				out2, _ := cmd2.Output()
+				fmt.Println(string(out2))
+				os.Remove(oldFile)
+				os.Remove(patchFile)
+				totalRetrieve -= 1
+				if totalRetrieve == 1 {
+					break
+				}
+				oldFile = tempFile
+				tempFile = strconv.Itoa(rand.Int())
+				patchFile = strconv.Itoa(totalRetrieve - 2)
+			}
+			retrieveName := file + "_v" + strconv.Itoa(version)
+			os.Rename(tempFile, retrieveName)
+		}
+	}
+}
+
+func (cli *CLI) Update(file string, version int, updateFile string) {
+	dir, _ := os.Getwd()
+	referencePath := filepath.Join(dir, "reference", file)
+	fmt.Println(referencePath)
+	_, err := os.Stat(referencePath)
+
+	if err != nil {
+		//reference file not found
+		fmt.Println("unable to update the file ", file, "you might give a wrong filename or need to use the upload command to upload it.")
+	} else {
+		//reference file found, update the file
+		//first, retrieve the base version of the file
+		cli.Retrieve(file, version)
+		//then, generate the patch
+		oldVersionName := file + "_v" + strconv.Itoa(version)
+		patchName := "patch" + strconv.Itoa(rand.Int())
+		cmd := exec.Command("bsdiff", oldVersionName, updateFile, patchName)
+		cmd.Output()
+		//next, import the patch to get its CID and upload it
+		cmd2 := exec.Command("./lotus", "client", "import", patchName)
+		out, _ := cmd2.Output()
+		//The output of this command follows the format Import XXXXXX, Root CID, we now spilt the string to get the CID
+		parts := strings.Split(string(out), "Root ")
+		if len(parts) < 2 {
+			fmt.Println("Error importing file")
+			return
+		}
+		cid := strings.TrimSpace(parts[1])
+		fmt.Println(cid)
+		cmd3 := exec.Command("./lotus", "client", "deal", cid, "t01000", "0.026", "518400")
+		out3, _ := cmd3.Output()
+		fmt.Println(string(out3))
+		//finally, update the reference file of the target file to save this update
+		filePrinter, _ := os.Open(referencePath)
+		defer filePrinter.Close()
+		//scan the file line by line and store it in an array
+		fileReference := []fileRef{}
+		scanner := bufio.NewScanner(filePrinter)
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+			parts := strings.Fields(line)
+			preVersion, _ := strconv.Atoi(parts[1])
+			reference := fileRef{fileCID: parts[0], previousVersion: preVersion}
+			fileReference = append(fileReference, reference)
+		}
+		fileReference = append(fileReference, fileRef{fileCID: cid, previousVersion: version})
+		versionNum := strconv.Itoa(len(fileReference))
+		WriteReference(file, fileReference)
+		fmt.Println("Updated ", file, "(version ", versionNum, ") to storage miner.")
+		os.Remove(oldVersionName)
+	}
+}
+
+func WriteReference(file string, fileReferences []fileRef) {
+	dir, _ := os.Getwd()
+	referencePath := filepath.Join(dir, "reference", file)
+	filePrinter, _ := os.Create(referencePath)
+	defer filePrinter.Close()
+	for _, ref := range fileReferences {
+		line := fmt.Sprintf("%s %d", ref.fileCID, ref.previousVersion)
+		filePrinter.WriteString(line)
+		filePrinter.WriteString("\n")
+	}
+}
--- cli/cli.go
+++ cli/cli.go
@@ -1,23 +1,14 @@
 package cli
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"math/rand"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"strconv"
 	"strings"
 )
 
 type CLI struct{}
-
-type fileRef struct {
-	fileCID         string
-	previousVersion int
-}
 
 type intslice []int
 
@@ -37,179 +28,23 @@ func (i *intslice) Set(value string) error {
 	return nil
 }
 
-func (cli *CLI) Upload(file string) {
-	dir, _ := os.Getwd()
-	referencePath := filepath.Join(dir, "reference", file)
-	fmt.Println(referencePath)
-	_, err := os.Stat(referencePath)
-
-	if err != nil {
-		//reference file not found, import it into FileDAG
-		cmd := exec.Command("./lotus", "client", "import", file)
-		out, _ := cmd.Output()
-		//The output of this command follows the format Import XXXXXX, Root CID, we now spilt the string to get the CID
-		parts := strings.Split(string(out), "Root ")
-		if len(parts) < 2 {
-			fmt.Println("Error importing file")
-			return
-		}
-		cid := strings.TrimSpace(parts[1])
-		fmt.Println(cid)
-		//use the CID of the file to upload it to the storage miner
-		cmd2 := exec.Command("./lotus", "client", "deal", cid, "t01000", "0.026", "518400")
-		out2, _ := cmd2.Output()
-		fmt.Println(string(out2))
-		//create the reference of the file to manage the state of the file
-		fileReference := []fileRef{}
-		fileReference = append(fileReference, fileRef{fileCID: cid, previousVersion: -1})
-		WriteReference(file, fileReference)
-		fmt.Println("Uploaded ", file, "(version 1) to storage miner.")
-	} else {
-		//reference file found, upload it to FileDAG
-		fmt.Println("the file ", file, "already exist in FileDAG. If you want to update it please use the update/fork/merge command.")
-	}
+func (cli *CLI) PrintUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("	upload -file filepath : upload the first version of a file to FileDAG, -file is the filepath of the upload file")
+	fmt.Println("	update -file filepath -base version -new filepath: update the version of a file, -file is the filename you input in the upload command, -base is the integer version number to apply the update -new is the filepath of the new version of the file.")
+	fmt.Println("	merge -file filepath -base version1,version2,... -new filepath: merge the version of a file, -file is the filename you input in the upload command, -base contains multiple integer version numbers to apply the nerge, saperated by comma -new is the filepath of the new version of the file.")
+	fmt.Println("	retrieve -file filepath -version version : retrieve a specific version of a file, -file is the filename you input in the upload command, -version is the integer version number you want to retrieve.")
 }
 
-func (cli *CLI) Retrieve(file string, version int) {
-	dir, _ := os.Getwd()
-	referencePath := filepath.Join(dir, "reference", file)
-	fmt.Println(referencePath)
-	_, err := os.Stat(referencePath)
-
-	if err != nil {
-		//reference file not found, the user might input a wrong filename or need to upload the file first
-		fmt.Println("unable to retrieve the file ", file, "you might give a wrong filename or need to use the upload command to upload it.")
-	} else {
-		//open the reference file to get the CID and previousVersion of each patch of the file
-		filePrinter, _ := os.Open(referencePath)
-		defer filePrinter.Close()
-		//scan the file line by line and store it in an array
-		fileReference := []fileRef{}
-		scanner := bufio.NewScanner(filePrinter)
-		for scanner.Scan() {
-			line := strings.TrimSpace(scanner.Text())
-			parts := strings.Fields(line)
-			preVersion, _ := strconv.Atoi(parts[1])
-			reference := fileRef{fileCID: parts[0], previousVersion: preVersion}
-			fileReference = append(fileReference, reference)
-		}
-		//test scan
-		for i := 0; i < len(fileReference); i++ {
-			fmt.Println(fileReference[i].fileCID, fileReference[i].previousVersion)
-		}
-		//test scan
-		//Now retrieve the target versions
-		versionToRetrieve := version
-		totalRetrieve := 0
-		for {
-			if versionToRetrieve == -1 {
-				break
-			}
-			cidToRetrieve := fileReference[versionToRetrieve-1].fileCID
-			retrieveFileName := strconv.Itoa(totalRetrieve)
-			cmd := exec.Command("./lotus", "client", "retrieve", cidToRetrieve, retrieveFileName)
-			out, _ := cmd.Output()
-			//test command exec
-			fmt.Println(string(out))
-			//test command exec
-			versionToRetrieve = fileReference[versionToRetrieve-1].previousVersion
-			totalRetrieve += 1
-		}
-		//Finally, put the original version and patches together
-		if totalRetrieve == 1 {
-			//retrieve the original file, just rename the file
-			retrieveName := file + "_v" + strconv.Itoa(version)
-			os.Rename(strconv.Itoa(0), retrieveName)
-		} else {
-			tempFile := strconv.Itoa(rand.Int())
-			oldFile := strconv.Itoa(totalRetrieve - 1)
-			patchFile := strconv.Itoa(totalRetrieve - 2)
-			for {
-				cmd2 := exec.Command("bspatch", oldFile, tempFile, patchFile)
-				out2, _ := cmd2.Output()
-				fmt.Println(string(out2))
-				os.Remove(oldFile)
-				os.Remove(patchFile)
-				totalRetrieve -= 1
-				if totalRetrieve == 1 {
-					break
-				}
-				oldFile = tempFile
-				tempFile = strconv.Itoa(rand.Int())
-				patchFile = strconv.Itoa(totalRetrieve - 2)
-			}
-			retrieveName := file + "_v" + strconv.Itoa(version)
-			os.Rename(tempFile, retrieveName)
-		}
-	}
-}
-
-func (cli *CLI) Update(file string, version int, updateFile string) {
-	dir, _ := os.Getwd()
-	referencePath := filepath.Join(dir, "reference", file)
-	fmt.Println(referencePath)
-	_, err := os.Stat(referencePath)
-
-	if err != nil {
-		//reference file not found
-		fmt.Println("unable to update the file ", file, "you might give a wrong filename or need to use the upload command to upload it.")
-	} else {
-		//reference file found, update the file
-		//first, retrieve the base version of the file
-		cli.Retrieve(file, version)
-		//then, generate the patch
-		oldVersionName := file + "_v" + strconv.Itoa(version)
-		patchName := "patch" + strconv.Itoa(rand.Int())
-		cmd := exec.Command("bsdiff", oldVersionName, updateFile, patchName)
-		cmd.Output()
-		//next, import the patch to get its CID and upload it
-		cmd2 := exec.Command("./lotus", "client", "import", patchName)
-		out, _ := cmd2.Output()
-		//The output of this command follows the format Import XXXXXX, Root CID, we now spilt the string to get the CID
-		parts := strings.Split(string(out), "Root ")
-		if len(parts) < 2 {
-			fmt.Println("Error importing file")
-			return
-		}
-		cid := strings.TrimSpace(parts[1])
-		fmt.Println(cid)
-		cmd3 := exec.Command("./lotus", "client", "deal", cid, "t01000", "0.026", "518400")
-		out3, _ := cmd3.Output()
-		fmt.Println(string(out3))
-		//finally, update the reference file of the target file to save this update
-		filePrinter, _ := os.Open(referencePath)
-		defer filePrinter.Close()
-		//scan the file line by line and store it in an array
-		fileReference := []fileRef{}
-		scanner := bufio.NewScanner(filePrinter)
-		for scanner.Scan() {
-			line := strings.TrimSpace(scanner.Text())
-			parts := strings.Fields(line)
-			preVersion, _ := strconv.Atoi(parts[1])
-			reference := fileRef{fileCID: parts[0], previousVersion: preVersion}
-			fileReference = append(fileReference, reference)
-		}
-		fileReference = append(fileReference, fileRef{fileCID: cid, previousVersion: version})
-		versionNum := strconv.Itoa(len(fileReference))
-		WriteReference(file, fileReference)
-		fmt.Println("Updated ", file, "(version ", versionNum, ") to storage miner.")
-		os.Remove(oldVersionName)
-	}
-}
-
-func WriteReference(file string, fileReferences []fileRef) {
-	dir, _ := os.Getwd()
-	referencePath := filepath.Join(dir, "reference", file)
-	filePrinter, _ := os.Create(referencePath)
-	defer filePrinter.Close()
-	for _, ref := range fileReferences {
-		line := fmt.Sprintf("%s %d", ref.fileCID, ref.previousVersion)
-		filePrinter.WriteString(line)
-		filePrinter.WriteString("\n")
+func (cli *CLI) ValidateArgs() {
+	if len(os.Args) < 2 {
+		cli.PrintUsage()
+		os.Exit(1)
 	}
 }
 
 func (cli *CLI) Run() {
+	cli.ValidateArgs()
 	uploadCmd := flag.NewFlagSet("upload", flag.ExitOnError)
 	retrieveCmd := flag.NewFlagSet("retrieve", flag.ExitOnError)
 	//file update and file fork process are almost the same, so I merge them into one command and make client to choose which version (updateBase) to apply the update
